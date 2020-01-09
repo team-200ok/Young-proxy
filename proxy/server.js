a9ec1b3a9a7902c5e49bc21e569fabd98c5afc4e
Young Cha <youngc0905@gmail.com>
2020-01-08 16:18:51 -0800

    implemented for instance

--- proxy/server.js
+++ proxy/server.js
@@ -1,9 +1,12 @@
+require('newrelic');
 const express = require('express');
 const path = require('path');
 // const bodyParser = require('body-parser');
 const app = express();
 const port = 3000;
 
+app.use('/', express.static(path.join(__dirname, 'public')));
 app.use('/:id', express.static(path.join(__dirname, 'public')));
+
 
 app.listen(port, () => console.log('Serving up fresh reviews on port', port));
